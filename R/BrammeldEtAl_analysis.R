2d9567e6a3a6ac83048985ca56f0e7ed35b38042
Francescojm <iorio@ebi.ac.uk>
2016-11-19 11:01:01 +0000

    brammeld et al pipeline saved

--- R/BrammeldEtAl_analysis.R
+++ R/BrammeldEtAl_analysis.R
@@ -3,12 +3,15 @@ source('R/SLAPenrich.R')
 load("data/SLAPE.all_genes_exonic_content_block_lengths_ensemble_20160209.RData")
 load('data/SLAPE.hgnc.table_20160210.Rdata')
 load('data/SLAPE.20140608_PATHCOM_HUMAN_nonredundant_intersection_hugoUpdated.Rdata')
-
-source('R/createJB_dataset.R')
+load('data/SLAPE.all_genes_exonic_lengths_ensemble_20160209.RData')
+variants<-read.table('data/BrammeldEtAl_variants.txt',sep='\t',row.names = 1,header=TRUE,stringsAsFactors = FALSE)
+Dataset<-
+    SLAPE.check_and_fix_gs_Dataset(variants,updated.hgnc.table = updated.hgnc.table)
 
 Dataset<-as.matrix(Dataset)
 
-PFPw<-SLAPE.Analyse(wBEM = Dataset,PATH_COLLECTION = PATHCOM_HUMAN,
+PFPw<-
+    SLAPE.analyse(EM = Dataset,PATH_COLLECTION = PATHCOM_HUMAN,
                     show_progress = TRUE,
                     NSAMPLES = 2,
                     NGENES = 2,
@@ -17,7 +20,8 @@ PFPw<-SLAPE.Analyse(wBEM = Dataset,PATH_COLLECTION = PATHCOM_HUMAN,
                     path_probability = 'Bernoulli',
                     GeneLenghts = GECOBLenghts)
 
-SLAPE.write.table(PFP = PFPw,BEM = Dataset,filename = "../../RESULTS/SLAPenrich/JB_paper.csv",
-                  fdrth=5,exclcovth = 50,PATH_COLLECTION = PATHCOM_HUMAN)
+SLAPE.write.table(PFP = PFPw,EM = Dataset,filename = "../../RESULTS/SLAPenrich/JonathanPaper/JB_paper_check.csv",
+                  fdrth=5,exclcovth = 50,PATH_COLLECTION = PATHCOM_HUMAN,GeneLenghts = GECOBLenghts)
+
 
 SLAPE.serialPathVis(BEM = Dataset,PFP = PFPw,fdrth = 5,exCovTh = 50,PATH = "../../RESULTS/SLAPenrich/JBp/",PATH_COLLECTION = PATHCOM_HUMAN)
